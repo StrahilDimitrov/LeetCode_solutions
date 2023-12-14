eb4e352737cf9027128a221daf7290991da9660c
Strahil Hristov <strahil1998@abv.bg>
2023-12-14 12:02:53 +0200

    Update Remove Duplicate from Sorted List.js

--- Remove Duplicate from Sorted List.js
+++ Remove Duplicate from Sorted List.js
@@ -1,34 +1,23 @@
-function remove(head) {
-    let result = [];
-
-    for (let i = 0; i < head.length; i++) {
-        for (let j = i + 1; j < head.length; j++) {
-            if(head[i] === head[j]){
-                head.splice(i, 1);
-            }
-        }
-        result.push(head[i]);
+function removeDublicate(head) {
+    if (head == null) {
+        return null;
     }
 
-    return result;
-}
+    let current = head;
 
-console.log(remove([1, 1, 2, 3, 3]));
+    while (current.next != null) {
+        let curVal = current.val;
+        let nextVal = current.next.val;
 
-console.log('----------');
-
-// Ready code
-function removeDuplicate(head) {
-
-    for (let i = 0; i < head.length; i++) {
-        for (let j = i + 1; j < head.length; j++) {
-            if (head[i] === head[j]) {
-                head.splice(i, 1);
-            }
+        if (curVal == nextVal) {
+            current.next = current.next.next;
+        }
+        else {
+            current = current.next
         }
     }
 
     return head;
 }
 
-console.log(removeDuplicate([1, 1, 2, 3, 3]));
+console.log(removeDublicate([1, 1, 2, 3, 3]));
